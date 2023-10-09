9754e2a8de08fc64b5f693e13e94c6e37be77f67
Roshan Aziz <roshan@Roshan-Aziz-TCE.local>
2023-10-09 17:57:59 +0500

    New changes

--- script.sh
+++ script.sh
@@ -1,58 +1,12 @@
 # Initialize a new Git repository in the current directory:
 git init
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
 mkdir -p .github/workflows
 
 touch .github/workflows/ci.yml
 
-<<<<<<< HEAD
-# Add all files in the current directory to the staging area:
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/main
-git pull origin main
 
-git merge --allow-unrelated-histories origin/main
 
-<<<<<<< HEAD
-=======
-=======
-git fetch origin main
-
-git merge --allow-unrelated-histories origin/main
-
-<<<<<<< HEAD
-=======
-=======
-<<<<<<< HEAD
-mkdir -p .github/workflow
-
-touch .github/workflows/ci.yml
-
-=======
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
-# Add all files in the current directory to the staging area:
-git add .
->>>>>>> origin/main
 
 # Sets the global user email configuration 
 # replace "you@something.com" with your own email id:
@@ -64,11 +18,11 @@ git config --global user.name "UserName"
 
 # Commits the changes to the local repository with a 
 # commit message of "test":
-<<<<<<< HEAD
+
 
 # Adds a remote repository named "origin" 
 # replace <remote repository URL> with your own gitlab URL:
-git remote add origin https://github.com/ed-roshan-aziz/Codefortests.git
+git remote add origin git@github.com:ed-roshan-aziz/Codefortests.git
 
 git pull origin main
 
@@ -77,39 +31,13 @@ git merge --allow-unrelated-histories origin/main
 git add .
 
 git commit -m "Running pipeline"
-=======
-git commit -m "Running pipeline"
+
 
 # Adds a remote repository named "origin" 
 # replace <remote repository URL> with your own gitlab URL:
-<<<<<<< HEAD
-git remote add origin https://github.com/ed-roshan-aziz/Codefortests.git
-=======
-<<<<<<< HEAD
-git remote add origin https://github.com/ed-roshan-aziz/Codefortests.git
-=======
-<<<<<<< HEAD
-git remote add origin https://github.com/ed-roshan-aziz/Codefortests.git
-=======
-<<<<<<< HEAD
-git remote add origin https://github.com/ed-roshan-aziz/Codefortests.git
-=======
-<<<<<<< HEAD
-git remote add origin https://github.com/ed-roshan-aziz/Codefortests.git
-=======
-git remote add origin https://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@github.com/ed-roshan-aziz/Codefortests.git
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
->>>>>>> origin/main
 
 
->>>>>>> origin/main
-# The command "git branch -M main" is used to 
-# rename the current branch to "main" in a Git repository:
-git branch -M main
 
 # Pushes the changes to the remote repository 
 # named "origin" on the "master" branch:
-git push -u origin main
+git push  origin main
